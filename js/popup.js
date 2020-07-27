940200d5623f5f8329fa6a293570ed8f3c749a7a
Oryps <oryps@localhost.localdomain>
2020-07-27 00:11:58 -0400

    footer y models terminados y funcionando

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,58 @@
-var btn_openPopup=document.getElementById("open-pop"),
-btn_closePopup=document.getElementById("close-pop"),
-overlay=document.getElementById("overlay");
+//BOTONES OPEN
 
-btn_openPopup.addEventListener("click",function(){
+var btnInfo=document.getElementById("openInfo-pop"),
+btnAgra=document.getElementById("openAgra-pop"),
+btnApo=document.getElementById("openApo-pop");
 
-    overlay.classList.add("active");
+
+//OVERLAYS
+var over1=document.getElementById("overlay-1"),
+over2=document.getElementById("overlay-2"),
+over3=document.getElementById("overlay-3");
+
+
+//MODELS
+
+var popInfo=document.getElementById("popInfo"),
+popAgra=document.getElementById("popAgra"),
+popApo=document.getElementById("popApo");
+
+//INFO
+
+btnInfo.addEventListener("click",function(){
+
+    over1.classList.add("active");
+    popInfo.classList.add("active");
 });
 
-btn_closePopup.addEventListener("click",function(){
-    overlay.classList.remove("active");
+over1.addEventListener("click",function(){
 
+    over1.classList.remove("active");
 });
 
-overlay.addEventListener("click",function(){
-    overlay.classList.remove("active");
+//AGRADECIMIENTOS
 
+btnAgra.addEventListener("click",function(){
+
+    over2.classList.add("active");
+    popAgra.classList.add("active");
 });
 
+over2.addEventListener("click",function(){
+
+    over2.classList.remove("active");
+});
+
+
+
+//APOYO
+
+btnApo.addEventListener("click",function(){
+
+    over3.classList.add("active");
+    popApo.classList.add("active");
+});
+
+over3.addEventListener("click", function(){
+    over3.classList.remove("active");
+})
